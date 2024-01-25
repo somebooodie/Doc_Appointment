c00a725405e75b6db2f4b0051ca9b6f6e5addfaf
Hamadbu <hamadbu97@gmail.com>
2024-01-25 09:48:35 +0300

    added an appbar

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_ar.dart
@@ -0,0 +1,24 @@
+import 'app_localizations.dart';
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get register => 'Register';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get userName => 'User Name';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get patientlogin => 'Patient Login';
+}
