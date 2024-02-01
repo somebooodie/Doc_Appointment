9102f7e2d7d4413023dd57772eeff924436aab09
Hamadbu <hamadbu97@gmail.com>
2024-02-01 11:02:15 +0300

    aa

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_ar.dart
@@ -0,0 +1,54 @@
+import 'app_localizations.dart';
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get login => 'تسجيل الدخول';
+
+  @override
+  String get register => 'التسجيل';
+
+  @override
+  String get email => 'البريد الإلكتروني';
+
+  @override
+  String get userName => 'اسم المستخدم';
+
+  @override
+  String get password => 'كلمة المرور';
+
+  @override
+  String get patientlogin => 'تسجيل الدخول كمريض';
+
+  @override
+  String get docLogin => 'Doctor Login';
+
+  @override
+  String get home => 'الرئيسية';
+
+  @override
+  String get calendar => 'التقويم';
+
+  @override
+  String get prescription => 'الوصفة الطبية';
+
+  @override
+  String get profile => 'Profile';
+
+  @override
+  String get changepassword => 'Change Password';
+
+  @override
+  String get patientregister => 'Patient Register';
+
+  @override
+  String get doctorId => 'Doctor Id';
+
+  @override
+  String get docRegister => 'Doctor Register';
+
+  @override
+  String get patientRegister => 'Patient Register';
+}
